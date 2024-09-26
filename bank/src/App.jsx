1066481fb33b1319a2ed5f233a7a20f647586aba
seya smoke <seyasmoke49@gmail.com>
2024-09-27 01:01:40 +0300

    init

---
+++ bank/src/App.jsx
@@ -0,0 +1,10 @@
+import styles from "./style"
+const App = () => (
+
+      <div className="bg-primary w-full overflow-hidden">
+        <div className={`${styles.paddingX}`}>
+        </div>
+      </div>
+
+)
+export default App
