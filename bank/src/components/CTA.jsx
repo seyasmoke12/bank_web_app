24ca1f10cb3121a3874950c7c41c67b2c85a0714
seya smoke <seyasmoke49@gmail.com>
2024-10-14 15:53:51 +0300

    clinets and cta

--- bank/src/components/CTA.jsx
+++ bank/src/components/CTA.jsx
@@ -1,8 +1,17 @@
-import React from 'react'
+import styles from "../style"
+import Button from "./Button"
 
 function CTA() {
   return (
-    <div>CTA</div>
+    <section className={`${styles.flexCenter} ${styles.marginY} ${styles.padding} sm:flex-row flex-col bg-black-gradient-2 rounded-[20px] box-shadow`}>
+      <div className="flex-1 flex flex-col">
+        <h2 className={styles.heading2}>Let's try our service now!</h2>
+        <p>Everythig you need to accept card payments and grow your business anywhere on the planet.</p>
+      </div>
+      <div>
+        <Button />
+      </div>
+    </section>
   )
 }
 
